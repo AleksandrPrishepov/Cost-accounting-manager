41b0a54c579e2f4aa7428db7d5ca498201cae55a
AleksandrPrishepov <sanyaprishepov89@gmail.com>
2022-11-05 09:25:48 +0300

    add name to models

--- costaccouting/yourcosts/models.py
+++ costaccouting/yourcosts/models.py
@@ -6,6 +6,9 @@ class User(AbstractUser):
     balance = models.DecimalField(blank=True, max_digits=10, decimal_places=2, verbose_name='баланс')
     categorys = models.ManyToManyField('Category')
 
+    def __str__(self):
+        return self.username
+
 class InfotmationTransaction(models.Model):
     costs_sum = models.DecimalField(blank=True, max_digits=10, decimal_places=2, verbose_name='cумма')
     time_operation = models.TimeField(auto_now_add=True,verbose_name='время')
@@ -14,7 +17,12 @@ class InfotmationTransaction(models.Model):
     description = models.TextField(max_length=250, verbose_name='описание')
     user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='пользователь')
 
+    def __str__(self):
+        return self.pk
+
 class Category(models.Model):
     name_cat = models.CharField(max_length=100, verbose_name='название категории')
 
+    def __str__(self):
+        return self.name_cat
 
